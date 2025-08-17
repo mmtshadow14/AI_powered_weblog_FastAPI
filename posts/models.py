b04452377a2fe96230b5241d8496a09eacdb7adb
Mohammad Mahdi Takfallah <mmtmmt945@gmail.com>
2025-08-17 17:53:35 +0330

    like route is ready to be test.

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,5 @@
 # SQlALCHEMY
-from sqlalchemy import Column, Integer, String, JSON
+from sqlalchemy import Column, Integer, String, JSON, ForeignKey
 
 # CORE MODELS
 from core.database import Base
@@ -14,3 +14,11 @@ class Post(Base):
     description = Column(String, nullable=False)
     tags = Column(JSON)
     likes = Column(Integer, default=0)
+
+
+class Like(Base):
+    __tablename__ = "likes"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user = Column(Integer, nullable=False)
+    post_id = Column(Integer, nullable=False)
--- posts/routes.py
+++ posts/routes.py
@@ -1,7 +1,10 @@
+# Python Packages
+import json
+
 # FastAPI models
-from fastapi import APIRouter, Cookie, Depends, HTTPException, status
+from fastapi import APIRouter, Cookie, Depends, HTTPException, status, Response
 from starlette.responses import JSONResponse
-from typing import List
+from typing import List, Optional
 
 # SQLALCHEMY
 from sqlalchemy.orm import Session
@@ -13,13 +16,16 @@ from core.database import get_db
 from auth.auth_token import retrieve_user_via_jwt
 
 # Posts App Models
-from posts.models import Post
+from posts.models import Post, Like
 
 # Posts App Schemas
 from posts.schemas import create_post_schemas, get_post_schemas
 
 # AI
 from AI.ai_funcs import get_keywords
+
+# utils
+from utils import check_like_status
 
 posts_router = APIRouter(prefix="/posts", tags=["posts"])
 
@@ -67,8 +73,53 @@ async def get_post_by_id(post_id: int, jwt_token: str = Cookie(None), db: Sessio
     """
     user = retrieve_user_via_jwt(jwt_token)
     if user and user.is_active:
-        post = db.query(Post).filter_by(Post.id == post_id).one_or_None()
+        post = db.query(Post).filter_by(id=post_id).one_or_none()
         if post:
             return post
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user not found')
+
+
+@posts_router.get('/like_post/{post_id}', )
+async def like_post(post_id: int, response: Response, jwt_token: str = Cookie(None), liked: Optional[str] = Cookie(default="[]"), db: Session = Depends(get_db)):
+    user = retrieve_user_via_jwt(jwt_token)
+    if user and user.is_active:
+        post = db.query(Post).filter_by(id=post_id).one_or_none()
+        if post:
+            like_status = check_like_status(user.id, post.id)
+            if like_status is not False:
+                db.delete(like_status)
+                db.commit()
+                return JSONResponse({'message': 'your like is removed successfully.'})
+            new_like_relation = Like(user=user.id, post_id=post.id)
+            liked_tags = json.loads(liked)
+            updated_tags = list(set(liked_tags + post.tags))
+            response.set_cookie(key="liked", value=json.dumps(updated_tags))
+            db.add(new_like_relation)
+            db.commit()
+            return JSONResponse({'message': 'your liked this post successfully.'})
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='post not found')
+    raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user not found')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
